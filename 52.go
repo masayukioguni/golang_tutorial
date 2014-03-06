5a7bb44d8879cbd43c351eb7fb2b308c4d9f36a4
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2014-03-07 01:27:14 +0900

    end

---
+++ 52.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func (v *Vertex) Abs() float64 {
+	fmt.Println(v)
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := &Vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(v, v.Abs())
+}
